1ce5013a25c45cce99a729539a8a0cb4f1a4f801
kurth4cker <kurth4cker@tutanota.com>
2022-09-19 14:16:59 +0300

    window.c: removed functions for raw trying

--- window.c
+++ window.c
@@ -1,12 +1,9 @@
 #include <ncurses.h>
 
-static WINDOW *create_newwin(int, int, int, int);
-static void destroy_win(WINDOW *);
-
 int
 main()
 {
-	WINDOW *my_win;
+	WINDOW *win;
 	int startx, starty, width, height;
 	int ch;
 
@@ -18,51 +15,39 @@ main()
 	width = 10;
 	starty = (LINES - height) / 2;
 	startx = (COLS - width) / 2;
+
 	printw("press `q' to exit");
 	refresh();
-	my_win = create_newwin(height, width, starty, startx);
-
-	while ((ch = getch()) != 'q') {
-		switch(ch) {
-		case KEY_LEFT:
-			destroy_win(my_win);
-			my_win = create_newwin(height, width, starty, --startx);
-			break;
-		case KEY_RIGHT:
-			destroy_win(my_win);
-			my_win = create_newwin(height, width, starty, ++startx);
-			break;
-		case KEY_UP:
-			destroy_win(my_win);
-			my_win = create_newwin(height, width, --starty, startx);
-			break;
-		case KEY_DOWN:
-			destroy_win(my_win);
-			my_win = create_newwin(height, width, ++starty, startx);
-			break;
-		}
-	}
-
-	endwin();
-	return 0;
-}
-
-static WINDOW *
-create_newwin(int height, int width, int starty, int startx)
-{
-	WINDOW *win;
 
 	win = newwin(height, width, starty, startx);
 	box(win, 0, 0);
 	wrefresh(win);
 
-	return win;
-}
+	while ((ch = getch()) != 'q') {
+		wborder(win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+		wrefresh(win);
+		delwin(win);
 
-static void
-destroy_win(WINDOW *win)
-{
-	wborder(win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-	wrefresh(win);
-	delwin(win);
+		switch(ch) {
+		case KEY_LEFT:
+			startx--;
+			break;
+		case KEY_RIGHT:
+			startx++;
+			break;
+		case KEY_UP:
+			starty--;
+			break;
+		case KEY_DOWN:
+			starty++;
+			break;
+		}
+
+		win = newwin(height, width, starty, startx);
+		box(win, 0, 0);
+		wrefresh(win);
+	}
+
+	endwin();
+	return 0;
 }
