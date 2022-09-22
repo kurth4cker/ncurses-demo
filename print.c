2e676b3836a1b530561b506cacf165580990f4cc
kurth4cker <kurth4cker@tutanota.com>
2022-09-22 13:12:15 +0300

    renamed printw.c to print.c
    print.c is a general output example so printw is not enough
    - added some extra strings
    - removed getmaxyx()

---
+++ print.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <ncurses.h>
+
+const char *addstrmesg = "string written by addstr";
+const char *printwmesg = "string written by printw";
+
+int
+main()
+{
+	int row, col;
+	int ch;
+
+	initscr();
+	raw();
+	keypad(stdscr, TRUE);
+	noecho();
+
+	mvaddstr(5, 5, addstrmesg);
+	mvprintw(10, 5, "%s", printwmesg);
+
+	move(15, 10);
+	addstr("write something ");
+	addstr("(press `q' to exit): ");
+	refresh();
+	while ((ch = getch()) != 'q')
+		addch(ch);
+	endwin();
+
+	return 0;
+}
