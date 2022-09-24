e2a5567710b4f9159eabf65b090d81dea1850284
kurth4cker <kurth4cker@tutanota.com>
2022-09-25 00:22:41 +0300

    chgat.c: more powerfull interaction

--- chgat.c
+++ chgat.c
@@ -4,12 +4,23 @@ int
 main()
 {
 	initscr();
+	noecho();
 	start_color();
 
 	init_pair(1, COLOR_BLUE, COLOR_BLACK);
-	printw("a big string which i didn't core to type fully");
-	move(0, 0);
-	chgat(-1, A_BLINK, 1, NULL);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
+	attron(COLOR_PAIR(2));
+	addstr("press any key to change following string's attributes");
+	attroff(COLOR_PAIR(2));
+
+	attron(A_BLINK);
+	move(3, 0);
+	addstr("a big string which i didn't core to type fully");
+	attroff(A_BLINK);
+
+	getch();
+	move(3, 0);
+	chgat(-1, A_BOLD, 1, NULL);
 	/*
 	 * first parameter number of characters to update. -1 means till
 	 *    end of line
@@ -20,6 +31,7 @@ main()
 	 * forth one is always NULL
 	 */
 	refresh();
+
 	getch();
 	endwin();
 	return 0;
