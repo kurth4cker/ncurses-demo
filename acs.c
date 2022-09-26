e2ba1ed5b60d36c36dcac48d8c6682db8085c9fd
kurth4cker <kurth4cker@tutanota.com>
2022-09-26 22:53:43 +0300

    acs.c: changed ugly syntax
    - printw() -> addstr()
    - added newlines after ';'
    - use addch() to print newline
    - hide cursor (curs_set()) and dont echo (noecho())

--- acs.c
+++ acs.c
@@ -4,38 +4,132 @@ int
 main()
 {
 	initscr();
+	curs_set(0);
+	noecho();
 
-	printw("upper left corner               "); addch(ACS_ULCORNER); printw("\n");
-	printw("lower left corner               "); addch(ACS_LLCORNER); printw("\n");
-	printw("lower right corner              "); addch(ACS_LRCORNER); printw("\n");
-	printw("tee pointing right              "); addch(ACS_LTEE); printw("\n");
-	printw("tee pointing left               "); addch(ACS_RTEE); printw("\n");
-	printw("tee pointing up                 "); addch(ACS_BTEE); printw("\n");
-	printw("tee pointing down               "); addch(ACS_TTEE); printw("\n");
-	printw("horizontal line                 "); addch(ACS_HLINE); printw("\n");
-	printw("vertical line                   "); addch(ACS_VLINE); printw("\n");
-	printw("large plus or cross over        "); addch(ACS_PLUS); printw("\n");
-	printw("scan line 1                     "); addch(ACS_S1); printw("\n");
-	printw("scan line 3                     "); addch(ACS_S3); printw("\n");
-	printw("scan line 7                     "); addch(ACS_S7); printw("\n");
-	printw("scan line 9                     "); addch(ACS_S9); printw("\n");
-	printw("diamond                         "); addch(ACS_DIAMOND); printw("\n");
-	printw("checker board (stipple)         "); addch(ACS_CKBOARD); printw("\n");
-	printw("degree symbol                   "); addch(ACS_DEGREE); printw("\n");
-	printw("plus/minus symbol               "); addch(ACS_PLMINUS); printw("\n");
-	printw("bullet                          "); addch(ACS_BULLET); printw("\n");
-	printw("arrow pointing left             "); addch(ACS_LARROW); printw("\n");
-	printw("arrow pointing right            "); addch(ACS_RARROW); printw("\n");
-	printw("arrow pointing down             "); addch(ACS_DARROW); printw("\n");
-	printw("arrow pointing up               "); addch(ACS_UARROW); printw("\n");
-	printw("board of squares                "); addch(ACS_BOARD); printw("\n");
-	printw("lantern symbol                  "); addch(ACS_LANTERN); printw("\n");
-	printw("solid square block              "); addch(ACS_BLOCK); printw("\n");
-	printw("less/equal sign                 "); addch(ACS_LEQUAL); printw("\n");
-	printw("greater/equal sign              "); addch(ACS_GEQUAL); printw("\n");
-	printw("pi                              "); addch(ACS_PI); printw("\n");
-	printw("not equal                       "); addch(ACS_NEQUAL); printw("\n");
-	printw("UK pound sign                   "); addch(ACS_STERLING); printw("\n");
+	addstr("upper left corner               ");
+	addch(ACS_ULCORNER);
+	addch('\n');
+
+	addstr("lower left corner               ");
+	addch(ACS_LLCORNER);
+	addch('\n');
+
+	addstr("lower right corner              ");
+	addch(ACS_LRCORNER);
+	addch('\n');
+
+	addstr("tee pointing right              ");
+	addch(ACS_LTEE);
+	addch('\n');
+
+	addstr("tee pointing left               ");
+	addch(ACS_RTEE);
+	addch('\n');
+
+	addstr("tee pointing up                 ");
+	addch(ACS_BTEE);
+	addch('\n');
+
+	addstr("tee pointing down               ");
+	addch(ACS_TTEE);
+	addch('\n');
+
+	addstr("horizontal line                 ");
+	addch(ACS_HLINE);
+	addch('\n');
+
+	addstr("vertical line                   ");
+	addch(ACS_VLINE);
+	addch('\n');
+
+	addstr("large plus or cross over        ");
+	addch(ACS_PLUS);
+	addch('\n');
+
+	addstr("scan line 1                     ");
+	addch(ACS_S1);
+	addch('\n');
+
+	addstr("scan line 3                     ");
+	addch(ACS_S3);
+	addch('\n');
+
+	addstr("scan line 7                     ");
+	addch(ACS_S7);
+	addch('\n');
+
+	addstr("scan line 9                     ");
+	addch(ACS_S9);
+	addch('\n');
+
+	addstr("diamond                         ");
+	addch(ACS_DIAMOND);
+	addch('\n');
+
+	addstr("checker board (stipple)         ");
+	addch(ACS_CKBOARD);
+	addch('\n');
+
+	addstr("degree symbol                   ");
+	addch(ACS_DEGREE);
+	addch('\n');
+
+	addstr("plus/minus symbol               ");
+	addch(ACS_PLMINUS);
+	addch('\n');
+
+	addstr("bullet                          ");
+	addch(ACS_BULLET);
+	addch('\n');
+
+	addstr("arrow pointing left             ");
+	addch(ACS_LARROW);
+	addch('\n');
+
+	addstr("arrow pointing right            ");
+	addch(ACS_RARROW);
+	addch('\n');
+
+	addstr("arrow pointing down             ");
+	addch(ACS_DARROW);
+	addch('\n');
+
+	addstr("arrow pointing up               ");
+	addch(ACS_UARROW);
+	addch('\n');
+
+	addstr("board of squares                ");
+	addch(ACS_BOARD);
+	addch('\n');
+
+	addstr("lantern symbol                  ");
+	addch(ACS_LANTERN);
+	addch('\n');
+
+	addstr("solid square block              ");
+	addch(ACS_BLOCK);
+	addch('\n');
+
+	addstr("less/equal sign                 ");
+	addch(ACS_LEQUAL);
+	addch('\n');
+
+	addstr("greater/equal sign              ");
+	addch(ACS_GEQUAL);
+	addch('\n');
+
+	addstr("pi                              ");
+	addch(ACS_PI);
+	addch('\n');
+
+	addstr("not equal                       ");
+	addch(ACS_NEQUAL);
+	addch('\n');
+
+	addstr("UK pound sign                   ");
+	addch(ACS_STERLING);
+	addch('\n');
 
 	refresh();
 	getch();
