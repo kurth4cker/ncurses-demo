80175021375d398978083104e4bc845a38f78f10
kurth4cker <kurth4cker@tutanota.com>
2022-09-26 22:45:13 +0300

    leaving.c: added some getch() breakpoints and blank lines

--- leaving.c
+++ leaving.c
@@ -5,15 +5,22 @@ int
 main()
 {
 	initscr();
+	curs_set(0);
+	noecho();
+
 	addstr("hello world\n");
 	refresh();
+	getch();
+
 	def_prog_mode();
 	endwin();
 	system("/bin/sh");
 	reset_prog_mode();
-	refresh();
+
 	addstr("another string");
 	refresh();
+
+	getch();
 	endwin();
 	return 0;
 }
