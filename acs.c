343ad7a5bbc4b3bab5441d7c506f889ee1c37e15
kurth4cker <kurth4cker@tutanota.com>
2022-10-07 18:21:25 +0300

    acs.c: add printing of ACS macro names

--- acs.c
+++ acs.c
@@ -1,44 +1,48 @@
 #include <ncurses.h>
 
+#define ARRSIZE(x) (sizeof(x)/sizeof(*x))
+
 struct ex {
 	char *str;
-	char acsvar;
+	char var;
+	char *macro_name;
 };
 typedef struct ex ex_t;
 
 ex_t acs_vars[] = {
-	{"upper left corner               ", 'l'},
-	{"lower left corner               ", 'm'},
-	{"upper right corner              ", 'k'},
-	{"lower right corner              ", 'j'},
-	{"tee pointing right              ", 't'},
-	{"tee pointing left               ", 'u'},
-	{"tee pointing up                 ", 'v'},
-	{"tee pointing down               ", 'w'},
-	{"horizontal line                 ", 'q'},
-	{"vertical line                   ", 'x'},
-	{"large plus or cross over        ", 'n'},
-	{"scan line 1                     ", 'o'},
-	{"scan line 3                     ", 'p'},
-	{"scan line 7                     ", 'r'},
-	{"scan line 9                     ", 's'},
-	{"diamond                         ", '`'},
-	{"checker board (stipple)         ", 'a'},
-	{"degree symbol                   ", 'f'},
-	{"plus/minus symbol               ", 'g'},
-	{"bullet                          ", '~'},
-	{"arrow pointing left             ", ','},
-	{"arrow pointing right            ", '+'},
-	{"arrow pointing down             ", '.'},
-	{"arrow pointing up               ", '-'},
-	{"board of squares                ", 'h'},
-	{"lantern symbol                  ", 'i'},
-	{"solid square block              ", '0'},
-	{"less/equal sign                 ", 'y'},
-	{"greater/equal sign              ", 'z'},
-	{"pi                              ", '{'},
-	{"not equal                       ", '|'},
-	{"UK pound sign                   ", '}'},
+	/* description,    SYMBOL in ncurses internal, macro name */
+	{"upper left corner               ", 'l', "ACS_ULCORNER"},
+	{"lower left corner               ", 'm', "ACS_LLCORNER"},
+	{"upper right corner              ", 'k', "ACS_URCORNER"},
+	{"lower right corner              ", 'j', "ACS_LRCORNER"},
+	{"tee pointing right              ", 't', "ACS_LTEE"},
+	{"tee pointing left               ", 'u', "ACS_RTEE"},
+	{"tee pointing up                 ", 'v', "ACS_BTEE"},
+	{"tee pointing down               ", 'w', "ACS_TTEE"},
+	{"horizontal line                 ", 'q', "ACS_HLINE"},
+	{"vertical line                   ", 'x', "ACS_VLINE"},
+	{"large plus or cross over        ", 'n', "ACS_PLUS"},
+	{"scan line 1                     ", 'o', "ACS_S1"},
+	{"scan line 3                     ", 'p', "ACS_S3"},
+	{"scan line 7                     ", 'r', "ACS_S7"},
+	{"scan line 9                     ", 's', "ACS_S9"},
+	{"diamond                         ", '`', "ACS_DIAMOND"},
+	{"checker board (stipple)         ", 'a', "ACS_CKBOARD"},
+	{"degree symbol                   ", 'f', "ACS_DEGREE"},
+	{"plus/minus symbol               ", 'g', "ACS_PLMINUS"},
+	{"bullet                          ", '~', "ACS_BULLET"},
+	{"arrow pointing left             ", ',', "ACS_LARROW"},
+	{"arrow pointing right            ", '+', "ACS_RARROW"},
+	{"arrow pointing down             ", '.', "ACS_DARROW"},
+	{"arrow pointing up               ", '-', "ACS_UARROW"},
+	{"board of squares                ", 'h', "ACS_BOARD"},
+	{"lantern symbol                  ", 'i', "ACS_LANTERN"},
+	{"solid square block              ", '0', "ACS_BLOCK"},
+	{"less/equal sign                 ", 'y', "ACS_LEQUAL"},
+	{"greater/equal sign              ", 'z', "ACS_GEQUAL"},
+	{"pi                              ", '{', "ACS_PI"},
+	{"not equal                       ", '|', "ACS_NEQUAL"},
+	{"UK pound sign                   ", '}', "ACS_STERLING"},
 };
 
 int
@@ -54,14 +58,15 @@ main()
 	while ((ch = getch()) != 'q') {
 		clear();
 		attron(A_BOLD);
-		move(sizeof(acs_vars)/sizeof(*acs_vars) + 2, 0);
+		move(ARRSIZE(acs_vars) + 2, 0);
 		addstr("press `q' to exit. another key to repeat");
 		attroff(A_BOLD);
 		move(0, 0);
 
 		for (i = 0; i < sizeof(acs_vars)/sizeof(*acs_vars); i++) {
 			addstr(acs_vars[i].str);
-			addch(NCURSES_ACS(acs_vars[i].acsvar));
+			addch(NCURSES_ACS(acs_vars[i].var));
+			printw("    %s", acs_vars[i].macro_name);
 			addch('\n');
 			ch = getch();
 			if (ch == 'q') {
